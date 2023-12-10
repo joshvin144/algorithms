9eb5710a8fc6e47fe79ff156f26af4dd98cc0905
joshvin144 <joshvin144@gmail.com>
2023-12-10 15:47:15 -0800

    [Addition] [adc] [Added comments]

--- adc/adc.h
+++ adc/adc.h
@@ -5,13 +5,14 @@
 #include <assert.h>
 
 
+// Sourced from "Figure 34. ADC accuracy characteristics" in the STM32L412KBU documentation
 #define VDDA ( 3.6f )
 #define VREF ( VDDA )
 #define ADC_RESOLUTION ( 12u )
 #define OUTPUT_CODE_RANGE ( ( uint32_t ) ( exp2( ADC_RESOLUTION ) ) ) 
 #define SINGLE_LSB ( VREF/ ( float ) OUTPUT_CODE_RANGE )
 
-
+// Structure to store the ADC characteristics necessary to convert the output code to voltage
 typedef struct{
 	float reference_voltage;
 	uint32_t output_code_range;
--- adc/adc_user.h
+++ adc/adc_user.h
@@ -1,7 +1,14 @@
 
 
+/*
+ * @description : Initializes the data structure in adc.c
+ * @parameters : None
+ */
 void adc_init_adc_module( void );
 
-
+/*
+ * @description : Calculates the voltage from the ADC output code
+ * @parameters : uint32_t output_code : The ADC output code between 0 and exp2( num_bits ), where num_bits is the number of ADC bits
+ */
 float adc_calculate_voltage_from_output_code( uint32_t output_code );
 
